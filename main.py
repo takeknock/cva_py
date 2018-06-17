bbbf027e6bd60d27b37c66c40b36265db55ed717
mmatthew <takeknock@gmail.com>
2018-06-17 17:44:04 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import QuantLib as Qlb
+
+
+def main():
+    # setting evaluation date
+    today = Qlb.Date(7, 4, 2015)
+    Qlb.Settings.instance().setEvaluationDate(today)
+
+    #
+
+
+
+
+
+if __name__ == '__main__':
+    main()
