c310b88fb27f0b9a1bebe3b2c902938a7da6654f
mmatthew <takeknock@gmail.com>
2018-06-17 20:59:56 +0900

    added sample cva calculation

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ def make_swap(start, maturity, notional, fixed_rate, index, typ=Qlb.VanillaSwap.
                            index.dayCounter())
 
     return swap, [index.fixingDate(x) for x in floater_leg_schedule][:-1]
+
 def main():
     # setting evaluation date
     today = Qlb.Date(7, 4, 2015)
@@ -161,11 +162,103 @@ def main():
     ax2.set_xlabel("Time in years")
     ax2.set_ylabel("NPV in time 0 Euros")
     ax2.set_title("Simulated discounted npv paths")
-    plt.show()
+    #plt.show()
 
+    print("portfolio_npv shape: ", portfolio_npv.shape)
+    # Calculating the positive expected exposure
+    E = portfolio_npv.copy()
+    E[E<0] = 0
+    EE = np.sum(E, axis=0) / N
 
+    dE = discounted_portfolio_npv.copy()
+    dE[dE<0] = 0
+    dEE = np.sum(dE, axis=0) / N
 
+    n = 30
+    f, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 10))
+    ax1.plot(time_grid, EE)
+    ax2.plot(time_grid, dEE)
+    ax1.set_xlabel("Time in years")
+    ax1.set_ylabel("Exposure")
+    ax1.set_title("Expected Exposure")
+    ax2.set_xlabel("Time in years")
+    ax2.set_ylabel("Discounted Exposure")
+    ax2.set_title("Discounted Expected Exposure")
 
+    plt.figure(figsize=(7, 5), dpi=300)
+    plt.plot(time_grid, dEE)
+    plt.xlabel("Time in years")
+    plt.ylabel("Discounting expected exposure")
+    plt.ylim([-2000, 10000])
+    plt.title("Expected exposure(netting set)")
+
+    # Calculating PFE curve(95% tile)
+    # pfe_curve = np.apply_along_axis(lambda x: np.sort(x)[0.95*N], 0, E)
+    # Traceback (most recent call last):
+    #   File "C:/Users/mmatt/01_work/25_xva/python/main.py", line 205, in <module>
+    #     main()
+    #   File "C:/Users/mmatt/01_work/25_xva/python/main.py", line 195, in main
+    #     pfe_curve = np.apply_along_axis(lambda x: np.sort(x)[0.95*N], 0, E)
+    #   File "C:\Users\mmatt\01_work\25_xva\python\venv\lib\site-packages\numpy\lib\shape_base.py", line 132, in apply_along_axis
+    #     res = asanyarray(func1d(inarr_view[ind0], *args, **kwargs))
+    #   File "C:/Users/mmatt/01_work/25_xva/python/main.py", line 195, in <lambda>
+    #     pfe_curve = np.apply_along_axis(lambda x: np.sort(x)[0.95*N], 0, E)
+    # IndexError: only integers, slices (`:`), ellipsis (`...`), numpy.newaxis (`None`) and integer or boolean arrays are valid indices
+    # plt.figure(figsize=(7, 5), dpi=300)
+    # plt.plot(time_grid, pfe_curve)
+    # plt.xlabel("Time in years")
+    # plt.ylabel("PFE")
+    # plt.ylim([-2000, 35000])
+    # plt.title("PFE (netting set)")
+    # plt.show()
+
+    # pfe = np.sort(np.max(E, axis=1))[0.95*N]
+    # Traceback (most recent call last):
+    #   File "C:/Users/mmatt/01_work/25_xva/python/main.py", line 217, in <module>
+    #     if __name__ == '__main__':
+    #   File "C:/Users/mmatt/01_work/25_xva/python/main.py", line 214, in main
+    #     # pfe = np.sort(np.max(E, axis=1))[0.95*N]
+    # IndexError: only integers, slices (`:`), ellipsis (`...`), numpy.newaxis (`None`) and integer or boolean arrays are valid indices
+    # print(pfe)
+
+    pd_dates = [today+Qlb.Period(i, Qlb.Years) for i in range(11)]
+    hzrates = [0.02 * i for i in range(11)]
+    pd_curve = Qlb.HazardRateCurve(pd_dates, hzrates, Qlb.Actual365Fixed())
+    pd_curve.enableExtrapolation()
+
+    # calculate default probabilities on grid times
+    times = np.linspace(0, 30, 100)
+    dp = np.vectorize(pd_curve.defaultProbability)(times)
+    sp = np.vectorize(pd_curve.survivalProbability)(times)
+    dd = np.vectorize(pd_curve.defaultDensity)(times)
+    hr = np.vectorize(pd_curve.hazardRate)(times)
+    f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(10, 10))
+    ax1.plot(times, dp)
+    ax2.plot(times, sp)
+    ax3.plot(times, dd)
+    ax4.plot(times, hr)
+    ax1.set_xlabel("Time in years")
+    ax2.set_xlabel("Time in years")
+    ax3.set_xlabel("Time in years")
+    ax4.set_xlabel("Time in years")
+    ax1.set_ylabel("Probability")
+    ax2.set_ylabel("Probability")
+    ax3.set_ylabel("Density")
+    ax4.set_ylabel("HazardRate")
+    ax1.set_title("Default Probability")
+    ax2.set_title("Survival Probability")
+    ax3.set_title("Default Density")
+    ax4.set_title("Hazard Rate")
+
+    # calculate
+    default_probabilities = np.vectorize(pd_curve.defaultProbability)
+    dPD = default_probabilities(time_grid[:-1], time_grid[1:])
+
+    # CVA
+    recovery_rate = 0.4
+    loss_given_default = 1 - recovery_rate
+    cva = loss_given_default * np.sum(dEE[1:] * dPD)
+    print("cva: ", cva)
 
 if __name__ == '__main__':
     main()
